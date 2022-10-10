3062c9b420610eaa9af0b46601c7492cc8fa424f
Jessica Taylor <jessi.liu.taylor@gmail.com>
2022-10-09 21:04:33 -0400

    some type checking

--- src/latgraph.ts
+++ src/latgraph.ts
@@ -16,3 +16,24 @@ interface LatGraph {
 
   transport(key: any, value: any, deps1: Deps, deps2: Deps): any;
 }
+
+function toLatGraph(v: any): LatGraph {
+  if (typeof v != 'object') {
+    throw new Error('Expected latgraph to be object');
+  }
+  let g = v as LatGraph;
+  if (typeof g.depGraphs != 'object') {
+    throw new Error('Expected depGraphs to be object');
+  }
+  for (const k in g.depGraphs) {
+    if (!Buffer.isBuffer(g.depGraphs[k])) {
+      throw new Error('Expected depGraphs to be buffers');
+    }
+  }
+  for (var f of [g.isKey, g.keyLt, g.isValue, g.bottom, g.join, g.transport]) {
+    if (typeof f != 'function') {
+      throw new Error('Expected function');
+    }
+  }
+  return g;
+}
